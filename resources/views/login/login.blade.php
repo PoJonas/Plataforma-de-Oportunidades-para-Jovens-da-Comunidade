0d1737ae94285c71b81757025399d66bbff752de
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-05-31 15:49:43 -0300

    Finalizando página de login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -6,27 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>login</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/login.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <form action="">
-        <div class="container-login">
+
+    <div class="container-login">
+        <div class="bem-vindo">
+            <img src="{{ asset('img/logo-foguete sem fundo.svg') }}" alt="">
+            <h4>Bem-vindo de volta</h4>
+            <span>Acesse sua conta para continuar</span>
+        </div>
+        <form action="">
+
             <div class="usuario">
-                <label for="usuario">Usuario:</label>
-                <input type="text" name="usuario" id="usuario" required placeholder=" seu@gmail.com">
+                <input type="text" name="usuario" id="usuario" required placeholder="">
+                <label for="usuario">Email</label>
             </div>
 
             <div class="senha">
-                <label for="senha">Senha:</label>
-                <input type="password" id="senha" name="senha" required placeholder="••••••••">
+                <input type="password" id="senha" name="senha" required placeholder="">
+                <label for="senha">Senha</label>
             </div>
 
             <div class="btn-login-container">
                 <button type="submit" class="btn-login">Entrar</button>
+
+                <a href="#" class="esqueciSenha">Esqueci a senha</a>
             </div>
+
+        </form>
+        <div class="cadastrar">
+            <span>Não tem conta?</span>
+            <a href="#">Cadastre-se</a>
         </div>
-    </form>
+    </div>
 </body>
 
 </html>
