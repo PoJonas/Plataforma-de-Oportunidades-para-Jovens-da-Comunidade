72818cc64043b170f4bc0e82066df5f169e54687
João <joaoao.0175@gmail.com>
2026-06-07 16:31:32 -0300

    Criação das migrations de Vagas, Cursos e Eventos

---
+++ database/migrations/2026_06_07_154240_add_vaga.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vagas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
+            $table->string('titulo', 60);
+            $table->text('descricao');
+            $table->text('requisitos');
+            $table->enum('regime', ['Presencial', 'Remoto', 'Hibrido']);
+            $table->enum('tipo_contrato', ['CLT', 'PJ', 'A combinar', 'Estágio']);
+            $table->string('carga_horaria', 50);
+            $table->string('salario', 100)->nullable();
+            $table->text('beneficios');
+            $table->enum('modalidade', ['Tempo Integral', 'Meio Período', 'Freelance']);
+            $table->boolean('vaga_pcd')->default(false);
+            $table->boolean('status')->default(true);
+            $table->integer('visualizacoes')->default(0);
+            $table->softDeletes('deleted_at')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vagas');
+    }
+};
